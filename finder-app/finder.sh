cb98207527c2de43cfd03d400accfaa276505d1d
Romanchukk <anton.romanenko14@ukr.net>
2026-03-17 21:59:17 +0200

    fix: add --text flag for grep

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -12,8 +12,8 @@ SRCHSTR=$2
 
 if [ -d $1 ]
 then
-    filenum=$(grep -r ${SRCHSTR}  "${FILESTR}" | cut -d: -f1 | sort | uniq | wc -l)
-    matchnum=$(grep -r ${SRCHSTR} "${FILESTR}" | wc -l)
+    filenum=$(grep -r --text ${SRCHSTR}  "${FILESTR}" | cut -d: -f1 | sort | uniq | wc -l)
+    matchnum=$(grep -r --text ${SRCHSTR} "${FILESTR}" | wc -l)
     echo "The number of files are ${filenum} and the number of matching lines are ${matchnum}"
 else
     echo "error: argument filesdir does not represent a directory on the filesystem"
